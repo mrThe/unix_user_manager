e20e97528290f9fb6386051aac4cc413ac8b49d5
mr.The <me@mrthe.name>
2022-10-20 17:28:35 +0300

    Add ability to set custom shell for user

--- lib/unix_user_manager/file/passwd.rb
+++ lib/unix_user_manager/file/passwd.rb
@@ -1,5 +1,5 @@
 class UnixUserManager::File::Passwd < UnixUserManager::File::Base
-  def add(name:, uid:, gid:)
+  def add(name:, uid:, gid:, shell: '/bin/bash')
     return false unless can_add?(name, uid)
     @new_records[name] = { uid: uid, gid: gid }
 
@@ -7,6 +7,6 @@ class UnixUserManager::File::Passwd < UnixUserManager::File::Base
   end
 
   def build_new_records
-    @new_records.map { |name, data| "#{name}:x:#{data[:uid]}:#{data[:gid]}::/dev/null:/bin/bash" }.join("\n")
+    @new_records.map { |name, data| "#{name}:x:#{data[:uid]}:#{data[:gid]}::/dev/null:#{shell}" }.join("\n")
   end
 end

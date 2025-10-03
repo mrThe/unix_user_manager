22f4d44c23aedcfcf67a9af9ca7657165d2e3a24
mr.The <me@mrthe.name>
2025-10-03 20:21:44 +0300

    Add ability to edit groups

--- lib/unix_user_manager/groups.rb
+++ lib/unix_user_manager/groups.rb
@@ -2,4 +2,8 @@ class UnixUserManager::Groups < UnixUserManager::Base
   def initialize(group:)
     @file = group
   end
+
+  def edit(name:, gid: nil, uname: nil)
+    file.edit(name: name, gid: gid, uname: uname)
+  end
 end

f8a745664210ead8cffe84c3a34a2d2a8eaeda30
mr.The <me@mrthe.name>
2025-10-03 13:33:14 +0300

    Bump bundler and fix typo for passwd file editor

--- lib/unix_user_manager/file/passwd.rb
+++ lib/unix_user_manager/file/passwd.rb
@@ -1,14 +1,14 @@
 class UnixUserManager::File::Passwd < UnixUserManager::File::Base
   def add(name:, uid:, gid:, home_directory: '/dev/null', shell: '/bin/bash')
     return false unless can_add?(name, uid)
-    @new_records[name] = { uid: uid, gid: gid }
+    @new_records[name] = { uid: uid, gid: gid, home_directory: home_directory, shell: shell }
 
     true
   end
 
   def build_new_records
     @new_records.map do |name, data|
-      "#{name}:x:#{data[:uid]}:#{data[:gid]}::#{home_directory}:#{shell}"
+      "#{name}:x:#{data[:uid]}:#{data[:gid]}::#{data[:home_directory]}:#{data[:shell]}"
     end.join("\n")
   end
 end

28ffa6f860c826cbaaa1927b8bd9102411dcabbb
mr.The <me@mrthe.name>
2025-10-05 17:09:27 +0300

    Add ability to edit shadow and passwords in it

--- lib/unix_user_manager/users.rb
+++ lib/unix_user_manager/users.rb
@@ -30,13 +30,17 @@ class UnixUserManager::Users < UnixUserManager::Base
     { passwd: build_passwd_new_records, shadow: build_shadow_new_records }
   end
 
-  def add(name:, uid:, gid:)
+  def add(name:, uid:, gid:, password: nil, encrypted_password: nil, salt: nil, algorithm: :sha512)
     return false unless file.can_add?(name, uid) && shadow_file.can_add?(name)
 
-    file.add(name: name, uid: uid, gid: gid) && shadow_file.add(name: name)
+    file.add(name: name, uid: uid, gid: gid) && shadow_file.add(name: name, password: password, encrypted_password: encrypted_password, salt: salt, algorithm: algorithm)
   end
 
   def edit(name:, uid: nil, gid: nil, home_directory: nil, shell: nil)
     file.edit(name: name, uid: uid, gid: gid, home_directory: home_directory, shell: shell)
+  end
+
+  def edit_shadow(name:, password: nil, encrypted_password: nil, salt: nil, algorithm: :sha512)
+    shadow_file.edit(name: name, password: password, encrypted_password: encrypted_password, salt: salt, algorithm: algorithm)
   end
 end
